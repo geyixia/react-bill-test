91d3b1ef18855dd69fa95cbbcc0da13f68353c90
葛东亚 <gdy_work_9464@163.com>
2023-12-27 08:46:37 +0800

    and主题定制&&react-redux

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,10 +1,24 @@
 
 import { Outlet } from 'react-router-dom'
+import { Button } from 'antd-mobile'
+import { useDispatch } from 'react-redux'
+import { useEffect } from 'react'
+import { getBillList } from '@/store/modules/billStore'
 const Layout = ()=>{
+    const dispatch = useDispatch()
+    useEffect(()=>{
+        dispatch(getBillList())
+    }, [dispatch])
+
     return (
         <div>
             Layout
             <Outlet />
+            {/* 测试全局生效样式 */}
+            <Button color='primary'>测试全局</Button>
+            <div className='purple-theme'>
+                <Button color='primary'>测试局部</Button>
+            </div>
         </div>
     )
 }
