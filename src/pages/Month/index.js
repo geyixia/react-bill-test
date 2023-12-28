572f872c3efe1264051d1c0eecce926ae500156c
葛东亚 <gdy_work_9464@163.com>
2023-12-28 09:09:50 +0800

    month

--- src/pages/Month/index.js
+++ src/pages/Month/index.js
@@ -1,8 +1,110 @@
+import { useSelector } from "react-redux"
+import { NavBar, DatePicker } from 'antd-mobile'
+import './index.scss'
+import _ from 'lodash'
+import dayjs from "dayjs"
+import { useMemo, useState } from "react"
+import { useEffect } from "react"
+import classNames from "classnames"
+import DailyBill from './components/DailyBill';
 
-const Month = ()=>{
-    return (
-        <div>Month</div>
-    )
+const Month = () => {
+  // 按月分组
+  const billList = useSelector(state => state.bill.billList)
+  const monthGroup = useMemo(() => {
+    return _.groupBy(billList, item => dayjs(item.date).format('YYYY-MM'))
+  }, [billList])
+
+  // 控制时间选择器打开关闭
+  const [dateVisible, setDateVisible] = useState(false)
+  const [currentMonthList, setMonthList] = useState([])
+  const [currentMonth, setCurrentMonth] = useState(() => {
+    return dayjs().format('YYYY-MM')
+  })
+
+  const dateConfirm = (date) => {
+    setDateVisible(false)
+    const monthKey = dayjs(date).format('YYYY-MM')
+    setCurrentMonth(monthKey)
+    setMonthList(monthGroup[monthKey])
+  }
+
+  // 首次加载
+  useEffect(() => {
+    const list = monthGroup[dayjs().format('YYYY-MM')]
+    if(list){
+      setMonthList(list)
+    }
+  }, [monthGroup])
+
+  // 计算统计
+  const overview = useMemo(() => {
+    if (!currentMonthList) return { income: 0, pay: 0, total: 0 }
+    const income = currentMonthList.filter(item => item.type === 'income')
+      .reduce((a, c) => a + c.money, 0)
+    const pay = currentMonthList.filter(item => item.type === 'pay')
+      .reduce((a, c) => a + c.money, 0)
+    return {
+      income,
+      pay,
+      total: income + pay
+    }
+  }, [currentMonthList])
+  // 把当前月按日分组账单数据
+  const dayGroup = useMemo(() => {
+    const group = _.groupBy(currentMonthList, (item) => dayjs(item.date).format('YYYY-MM-DD'))
+    return {
+      dayKeys: Object.keys(group),
+      group
+    }
+  }, [currentMonthList])
+
+  return (
+    <div className="monthlyBill">
+      <NavBar className="nav" backArrow={false}>
+        月度收支
+      </NavBar>
+      <div className="content">
+        <div className="header">
+          {/* 时间切换区域 */}
+          <div className="date" onClick={() => setDateVisible(true)}>
+            <span className="text">
+              {currentMonth} 账单
+            </span>
+            <span className={classNames('arrow', dateVisible && 'expand')}></span>
+          </div>
+          {/* 统计区域 */}
+          <div className='twoLineOverview'>
+            <div className="item">
+              <span className="money">{overview.pay.toFixed(2)}</span>
+              <span className="type">支出</span>
+            </div>
+            <div className="item">
+              <span className="money">{overview.income.toFixed(2)}</span>
+              <span className="type">收入</span>
+            </div>
+            <div className="item">
+              <span className="money">{(overview.total).toFixed(2)}</span>
+              <span className="type">结余</span>
+            </div>
+          </div>
+          {/* 时间选择器 */}
+          <DatePicker
+            className="kaDate"
+            title="记账日期"
+            precision="month"
+            visible={dateVisible}
+            max={new Date()}
+            onCancel={()=>{setDateVisible(false)}}
+            onConfirm={dateConfirm}
+          />
+        </div>
+        {dayGroup.dayKeys.map(dayKey => (
+            <DailyBill key={dayKey} date={dayKey} billList={dayGroup.group[dayKey]} />
+        ))}
+      </div>
+    </div >
+  )
 }
 
 export default Month
